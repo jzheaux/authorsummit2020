ccbe8e2f812c2accd3892a150aed824413ddb839
Josh Cummings <josh.cummings@gmail.com>
2020-09-14 17:55:47 -0600

    Add User to Message
    You can ask for the currently logged in user by adding Authentication
    as a Spring MVC controller's method parameter.

--- src/main/java/io/jzheaux/springsecurity/authors/AuthorsController.java
+++ src/main/java/io/jzheaux/springsecurity/authors/AuthorsController.java
@@ -1,13 +1,14 @@
 package io.jzheaux.springsecurity.authors;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthorsController {
 	@GetMapping("/")
-	public String hello() {
-		String name = "Authors";
+	public String hello(Authentication authentication) {
+		String name = authentication.getName();
 		return String.format("Hello, %s!", name);
 	}
 }
